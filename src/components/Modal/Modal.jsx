6ee583523d7e216644c5be25e6f49a6a9f68111b
Annarchive <ytpt@mail.ru>
2023-06-14 19:32:47 +0400

    Added close button to Modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,16 +1,26 @@
 import React from "react";
-import { EnterForm } from "../EnterForm";
+import { EnterForm } from "../EnterForm/EnterForm";
 import "./Modal.css";
+import { AiOutlineClose } from "react-icons/ai";
 
 export function Modal( { onClose } )  {
     return (
         <>
             <div className="modal-bg" onClick={onClose} />
             <div className="modal-container">
-                <h1 className="modal-title">
-                    Web <span>App</span>
-                </h1>
-                <EnterForm onCreate={ onClose } title={"Войдите в свой профиль"} />
+                <div className="modal-header">
+                    <h1 className="modal-title">
+                        Web <span>App</span>
+                    </h1>
+                    <AiOutlineClose
+                        className="modal-close-btn"
+                        onClick={onClose}
+                    />
+                </div>
+                <EnterForm
+                    onCreate={onClose}
+                    title={"Войдите в свой профиль"}
+                />
             </div>
         </>
     )
