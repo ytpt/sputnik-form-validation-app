9b7661b9f99c6af5d2c3ce2a9e67f496828a45ba
Annarchive <ytpt@mail.ru>
2023-06-15 20:02:17 +0400

    Refactored code style

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,30 +1,36 @@
 import React from "react";
 import { EnterForm } from "../EnterForm/EnterForm";
 import "./Modal.css";
-import { AiOutlineClose } from "react-icons/ai";
-import {Link} from "react-router-dom";
+import { AiOutlineClose, AiOutlineMail } from "react-icons/ai";
+import { Link } from "react-router-dom";
 
-export function Modal( { onClose } )  {
+export function Modal({ onClose })  {
     return (
         <>
-            <div className="modal-bg" onClick={onClose} />
-            <div className="modal-container">
+            <div className="modal-bg" onClick={ onClose } />
+            <div className="modal">
                 <div className="modal-header">
-                    <h1 className="modal-title">
+                    <h1 className="modal-header__title">
                         Web <span>App</span>
                     </h1>
-                    <AiOutlineClose
-                        className="modal-close-btn"
-                        onClick={onClose}
-                    />
+                    <AiOutlineClose className="modal-header__btn" onClick={ onClose } />
                 </div>
-                <EnterForm
-                    onCreate={onClose}
-                    title={"Войдите в свой профиль"}
-                />
+                <EnterForm onCreate={ onClose } title={ "Войдите в свой профиль" } />
                 <div className="modal-links">
-                    <Link to="/password" className="modal-link">Я забыл пароль</Link>
-                    <Link to="/registration" className="modal-link">Регистрация</Link>
+                    <Link
+                        to="/password"
+                        className="modal-links__link">
+                        Я забыл пароль
+                    </Link>
+                    <Link
+                        to="/registration"
+                        className="modal-links__link">
+                        Регистрация
+                    </Link>
+                </div>
+                <div className="modal-support">
+                    <AiOutlineMail className="modal-support__icon" />
+                    <p className="modal-support__text">Написать в поддержку</p>
                 </div>
             </div>
         </>
