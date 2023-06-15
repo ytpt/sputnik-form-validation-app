29c592f83e2b1d84f37d7bf607f01483b9d6d067
Annarchive <ytpt@mail.ru>
2023-06-15 20:03:28 +0400

    Added elements to PasswordPage and RegistrationPage

--- src/pages/PasswordPage.jsx
+++ src/pages/PasswordPage.jsx
@@ -3,15 +3,14 @@ import "../components/Modal/Modal.css";
 import { AiOutlineClose } from "react-icons/ai";
 import { Link } from "react-router-dom";
 
-export function RegistrationPage({ handleModalBgClick }) {
-
+export function PasswordPage({ handleModalBgClick }) {
     return (
         <>
             <div className="modal-bg" onClick={ handleModalBgClick } />
             <div className="modal">
                 <div className="modal-header">
                     <h2 className="modal-header__title">
-                        Страница пока не готова :(
+                        Что-то пошло не так :(
                     </h2>
                     <Link to="/">
                         <AiOutlineClose className="modal-header__btn back-btn" />
